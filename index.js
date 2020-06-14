761e9c3013ffa88a7122eba71b1520e5575a7b0d
Tom Atkinson <tja.atkinson@gmail.com>
2020-06-14 23:37:32 +0100

    Added Logging

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-const fetch = require('node-fetch')
-const core = require('@actions/core');
-const { Webhooks } = require("@octokit/webhooks")
+const fetch = require("node-fetch");
+const core = require("@actions/core");
+const { Webhooks } = require("@octokit/webhooks");
 
 try {
+  const URL = core.getInput("URL", { required: true });
+  const PAT = core.getInput("PAT", { required: true });
 
-    const URL = core.getInput('URL', { required: true })
-    const PAT = core.getInput('PAT', { required: true })
+  const webhook = new Webhooks({ secret: PAT });
 
-    const webhook = new Webhooks({ secret: PAT })
+  let DATA = {};
 
-    let DATA = {}
+  try {
+    DATA = JSON.parse(core.getInput("PAYLOAD"));
+  } catch (err) {
+    core.setFailed("The payload passed is not valid JSON");
+  }
 
-    try {
-        DATA = JSON.parse(core.getInput('PAYLOAD'))
-    } catch (err) {
-        core.setFailed("The payload passed is not valid JSON");
-    }
+  const PAYLOAD = {
+    ...DATA,
+  };
 
-    const PAYLOAD = {
-        ...DATA
-    }
+  const SIGNITURE = webhook.sign(PAYLOAD);
 
-    const SIGNITURE = webhook.sign(PAYLOAD)
-
-    fetch(URL, {
-        method: "POST",
-        headers: {
-            "X-Hub-Signature": SIGNITURE,
-            "Authorization": `token ${PAT}`,
-            "Content-Type": "application/json",
-            "Accept": "application/vnd.github.everest-preview+json"
-        },
-        body: PAYLOAD
-    })
+  fetch(URL, {
+    method: "POST",
+    headers: {
+      "X-Hub-Signature": SIGNITURE,
+      Authorization: `token ${PAT}`,
+      "Content-Type": "application/json",
+      Accept: "application/vnd.github.everest-preview+json",
+    },
+    body: PAYLOAD,
+  })
+    .then((res) => res.json())
+    .then((res) => core.info(res));
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
